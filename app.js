d222ee9bd3c0e22d6f697ca395009f55f6886e3b
Park-Seaweed <qkralsgur721@gmail.com>
2023-01-18 00:19:59 +0900

    mod: app.js 수정

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hellow World');
+  res.send('Hellow World!!-ecr-test');
 });
 app.listen(port, () => {
   console.log(`listening ar:${port} `);
