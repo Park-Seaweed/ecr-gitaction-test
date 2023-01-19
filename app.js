79040d3b224550e713a1fe02309a544e95a03817
Park-Seaweed <qkralsgur721@gmail.com>
2023-01-19 23:33:59 +0900

    add: 사용하는 서버 IP확인 로직 작성

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
-
 const app = express();
-
 const port = 8080;
+const os = require('os');
 
 app.get('/', (req, res) => {
-  res.send('Hellow World!!-ecr-test');
+  const interfaces = os.networkInterfaces();
+  let address;
+
+  Object.keys(interfaces).forEach((interfaceName) => {
+    interfaces[interfaceName].forEach((interface) => {
+      if (interface.family === 'IPv4' && !interface.internal) {
+        address = interface.address;
+      }
+    });
+  });
+
+  res.status(200).send(`이 서버의 IP주소는:${address}입니다`);
 });
+
 app.listen(port, () => {
-  console.log(`listening ar:${port} `);
+  console.log(`listening port:${port} `);
 });
