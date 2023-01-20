64773e58439584f75d2168e5aa9a0752a180f33b
Park-Seaweed <qkralsgur721@gmail.com>
2023-01-20 15:38:52 +0900

    add: yaml 수정

--- ecr-test/routes/root.js
+++ ecr-test/routes/root.js
@@ -1,7 +1,18 @@
-'use strict'
-
+'use strict';
+const os = require('os');
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
-}
+    const interfaces = os.networkInterfaces();
+    let address;
+
+    Object.keys(interfaces).forEach((interfaceName) => {
+      interfaces[interfaceName].forEach((interface) => {
+        if (interface.family === 'IPv4' && !interface.internal) {
+          address = interface.address;
+        }
+      });
+    });
+
+    reply.code(200).send(`이서버의 IP는:${address}`);
+  });
+};
