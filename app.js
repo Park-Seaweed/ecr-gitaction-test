d9802b77b1bbd3703b1ec6faf7b96dfba2792785
Park-Seaweed <qkralsgur721@gmail.com>
2023-01-20 13:58:59 +0900

    add: 목데이터 삽입

--- app.js
+++ app.js
@@ -4,18 +4,7 @@ const port = 8080;
 const os = require('os');
 
 app.get('/', (req, res) => {
-  const interfaces = os.networkInterfaces();
-  let address;
-
-  Object.keys(interfaces).forEach((interfaceName) => {
-    interfaces[interfaceName].forEach((interface) => {
-      if (interface.family === 'IPv4' && !interface.internal) {
-        address = interface.address;
-      }
-    });
-  });
-
-  res.status(200).ok(`이 서버의 IP주소는:${address}입니다`);
+  res.json({ data: 'hello world' });
 });
 
 app.listen(port, () => {
